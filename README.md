3fba3772d3051cd2e8470b89d25cb7384fde1e4c
Vishwanath <vishwa.hyd@gmail.com>
2016-06-13 22:33:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ewaste 3D Printer
+3D Printer based on discarded CD drives. The project is more or less copied from
+http://www.instructables.com/id/eWaste-60-3DPrinter/
+
+## Mechanics
+1. 2 stepper motor based CD drives.
+2. 1 DC motor based CD drive.
+3. 1 stepper motor based extruder.
+4. Chasis made with wood.
+
+## Hardware
+1. Teensy LC
+2. Pololu 8843 drivers for stepper motors
+3. L293D for DC motors
+
+## Software
+1. Teensy template at http://github.com/apmorton/teensy-template
+2. Python HID module at https://pypi.python.org/pypi/hid
+
+
---
+++ src/main.cpp
@@ -0,0 +1,73 @@
+//#include "WProgram.h"
+#include <avr/pgmspace.h>
+#include <avr/interrupt.h>
+
+#include <avr_functions.h>
+#include <avr_emulation.h>
+
+#include <usb_dev.h>
+#include <usb_rawhid.h>
+
+#include <IntervalTimer.h>
+
+// Preprocessor macros
+#define TIMEOUT_RECV 	0 		// USB receive timeout.
+#define TIMEOUT_SEND 	50 		// USB send timeout.
+#define LED 			13 		// LED pin number.
+
+// USB buffer
+uint8_t usb_buffer[64];
+
+// Create an LED blink timer object.
+IntervalTimer led_timer;
+int led_state = LOW;
+void led_blink_func(void);
+
+int main(void)
+{
+	// Variables
+	uint8_t delay_val = 255;
+	uint8_t nbytes;
+
+	// Start timer.
+	led_timer.begin(led_blink_func, delay_val);
+
+	// Initialize USB.
+	usb_init();
+
+	// I want to control an LED.
+	pinMode(LED, OUTPUT);
+
+	// Halt till the device configures itself.
+	while(!usb_rawhid_available());
+
+	// Give some time for the host to settle as well.
+	delay(1000);
+
+	while (1)
+	{
+		// Receive and echo the packet.
+		nbytes = usb_rawhid_recv(usb_buffer, TIMEOUT_RECV);
+		
+		if (nbytes > 0)
+		{
+			// Read the delay between blinks
+			delay_val = usb_buffer[0];
+
+			// Restart timer.
+			led_timer.end();
+			led_timer.begin(led_blink_func, delay_val*1000);
+
+		}
+	}
+}
+
+void led_blink_func(void)
+{
+	if(led_state == LOW)
+		led_state = HIGH;
+	else
+		led_state = LOW;
+
+	digitalWrite(LED, led_state);
+}
